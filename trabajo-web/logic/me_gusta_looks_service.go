ce699b94a4691b6298bb9e917a0332f55cf50369
FedeG2406 <fgraziano@alumnos.exa.unicen.edu.ar>
2026-09-23 18:15:12 -0300

    Refactor AddMeGusta and DeleteMeGusta methods
    Refactor MeGustaLooksService to use CreateMeGustaParams struct and remove error checks for valid IDs.

--- trabajo-web/logic/me_gusta_looks_service.go
+++ trabajo-web/logic/me_gusta_looks_service.go
@@ -2,7 +2,6 @@ package logic
 
 import (
 	"context"
-	"errors"
 
 	"trabajo-web/db/sqlc"
 )
@@ -12,32 +11,22 @@ type MeGustaLooksService struct {
 }
 
 func NewMeGustaLooksService(queries *sqlc.Queries) *MeGustaLooksService {
-	return &MeGustaLooksService{
-		queries: queries,
-	}
+	return &MeGustaLooksService{queries: queries}
 }
 
-type MeGustaParams struct {
+type CreateMeGustaParams struct {
 	UsuarioID int32 `json:"usuario_id"`
 	LookID    int32 `json:"look_id"`
 }
 
-func (s *MeGustaLooksService) AddMeGusta(ctx context.Context, params MeGustaParams) error {
-	if params.UsuarioID <= 0 || params.LookID <= 0 {
-		return errors.New("los ID de usuario y look deben ser válidos")
-	}
-
+func (s *MeGustaLooksService) AddMeGusta(ctx context.Context, params CreateMeGustaParams) error {
 	return s.queries.DarMeGusta(ctx, sqlc.DarMeGustaParams{
 		UsuarioID: params.UsuarioID,
 		LookID:    params.LookID,
 	})
 }
 
-func (s *MeGustaLooksService) DeleteMeGusta(ctx context.Context, params MeGustaParams) error {
-	if params.UsuarioID <= 0 || params.LookID <= 0 {
-		return errors.New("los ID de usuario y look deben ser válidos")
-	}
-
+func (s *MeGustaLooksService) DeleteMeGusta(ctx context.Context, params CreateMeGustaParams) error {
 	return s.queries.QuitarMeGusta(ctx, sqlc.QuitarMeGustaParams{
 		UsuarioID: params.UsuarioID,
 		LookID:    params.LookID,
